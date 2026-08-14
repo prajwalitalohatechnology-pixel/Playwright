7609edac75584038a089e320056bd4ee48607d35
prajwalitalohatechnology-pixel <prajwalitalohatechnology@gmail.com>
2026-08-14 10:02:15 -0400

    first commit

--- src/tests/jenkins.test.ts
+++ src/tests/jenkins.test.ts
@@ -3,8 +3,4 @@ import { test, expect } from '@playwright/test';
 test('Google title', async ({ page }) => {
   await page.goto('https://www.google.com');
   await expect(page).toHaveTitle(/Google/);
-  await page.locator('textarea[name="q"]').fill('Search');
-  await page.locator('textarea[name="q"]').press('Enter');
-  await expect(page).toHaveTitle(/Search - Google/);
-
 });
