041e7baa4ebd8906079b7f07e18bc2dcf6e9b096
prajwalitalohatechnology-pixel <prajwalitalohatechnology@gmail.com>
2026-08-14 09:48:16 -0400

    Trigger Jenkins

--- src/tests/jenkins.test.ts
+++ src/tests/jenkins.test.ts
@@ -3,4 +3,9 @@ import { test, expect } from '@playwright/test';
 test('Google title', async ({ page }) => {
   await page.goto('https://www.google.com');
   await expect(page).toHaveTitle(/Google/);
+  await page.locator('textarea[name="q"]').fill('Search');
+  await page.locator('textarea[name="q"]').press('Enter');
+  await page.waitForNavigation();
+  await expect(page).toHaveTitle(/Search - Google/);
+
 });
